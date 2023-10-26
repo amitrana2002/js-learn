3cf2ea29137070ba472d57d0f4b56ef093750ab0
amitrana2002 <85242623+amitrana2002@users.noreply.github.com>
2023-10-26 14:27:01 +0000

    datatypes

---
+++ basics/2_datatypes.js
@@ -0,0 +1,22 @@
+"use Strict" // treat all js code as newer version
+// alert("Hello"); //we are using nodejs not browser
+
+/*
+number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness
+*/
+
+let a=2002; // number
+let big_a = 98459437;
+let name="Amit"; //string
+let study=true; //bool
+let state;
+let blank=null;
+
+console.table([typeof(a), typeof(big_a),typeof(name), typeof(study),typeof(state),typeof(blank)]);
+
