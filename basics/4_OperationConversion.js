89c7d1d736c2a98b672f15f9d3ddec3dad48a840
amitrana2002 <85242623+amitrana2002@users.noreply.github.com>
2023-10-27 13:28:43 +0000

    operation Conversion

---
+++ basics/4_OperationConversion.js
@@ -0,0 +1,16 @@
+let str1="Amit";
+let str2=" Rana"
+
+let str3 = str1+str2;
+console.log(str3);
+
+//jo datatype phle aaega usme convert hoga sara
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+3); //123
+console.log(1 + 2 + "3"); //33
+console.log(1+2+3); //6
+console.log(("1")+2+3); //123
+
+
+
