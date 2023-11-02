dca9de81f8e0e893d7cd809493a3b3a1863d818a
amitrana2002 <85242623+amitrana2002@users.noreply.github.com>
2023-11-02 18:15:10 +0000

    functions

---
+++ functions/1_function.js
@@ -0,0 +1,19 @@
+
+//simple function
+function myname(){
+    console.log("Amit Rana");
+}
+myname();
+
+
+// function with argument
+function addnum(a,b)
+{
+    console.log(a+b);
+}
+addnum(5,4); // if no argument then function will print NaN
+
+addnum(5,"4"); // it will return 54
+
+addnum(3,"a")//3a
+
