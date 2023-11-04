62024f6ac8a4b0d905696a6999cde9db9dae2a63
amitrana2002 <85242623+amitrana2002@users.noreply.github.com>
2023-11-04 16:32:38 +0000

    controlflow

---
+++ 6controlflow/one.js
@@ -0,0 +1,28 @@
+// const Userlogged=true;
+
+// if(Userlogged==true)
+// {
+//     console.log("User Logged In");
+// }
+
+// if(2=="2") //only value check
+// {
+//     console.log("Double equal to");
+// }
+
+// if(2==="2") // triple equal to also checks datatype along with values
+// {
+//     console.log("Triple equal to ");
+// }
+
+//if else
+const temperature=50;
+if(temperature<=50)
+{
+    console.log("Temperature is not high");
+}
+else{
+    console.log("Temperature is very high");
+}
+
+
