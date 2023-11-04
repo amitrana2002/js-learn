3ce0ddf527451601332910e414e9855d8f3d5776
amitrana2002 <85242623+amitrana2002@users.noreply.github.com>
2023-11-04 16:45:05 +0000

    switch

---
+++ 6controlflow/switch.js
@@ -0,0 +1,47 @@
+ //syntax
+// switch(key)
+// {
+//     case value:
+
+//     break;
+
+//     default;
+
+//     break;
+// }
+
+const date = 4;
+
+switch(date)
+{
+    case 4:
+        console.log("Today is 4th November");
+        break;
+   
+    case 5:
+        console.log("Today is 5th November");
+        break;
+
+    default:
+        console.log("No date Matched");
+        break;
+}
+
+
+const date2 = "Fourth";
+
+switch(date2)
+{
+    case "Fifth":
+        console.log("Today is Fifth November");
+        break;
+   
+    case "Fourth":
+        console.log("Today is Fourth November");
+        break;
+        
+    default:
+        console.log("No date Matched");
+        break;
+}
+
